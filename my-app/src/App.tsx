aae35e133a26c3df68309a7848b6bf1c41396e10
edumiguel <dudumfmonteiro@gmail.com>
2024-08-13 09:53:53 -0300

    Corrigindo ex-03

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+export default function App(){
+//Area declarativa
+//Criando um array com 10 salários
+const salarios = [1000,2000,3000,4000,5000,6000,1500,1800,2200,3600]
+
+
+//Atribuindo um aumento de 15% para salario até 2000 e um aumento de 10% para salarios acima de 2000.
+const salComAumento = salarios.map(salario => salario<= 2000 ? salario+(salario * 0.15) : salario+(salario * 0.10));
+console.log(salComAumento)
+
+//Filtrando Salarios acima de 2500
+const salMaior2500 = salComAumento.filter(salario => salario > 2500);
+console.log(salMaior2500);
+
+//Somando os salarios
+const salSoma = salMaior2500.reduce((acumulado,salario)=>{return(acumulado+salario)},0)
+console.log(salSoma)
+
+  return( 
+    <>{/*Fragment*/}
+      <p>Olá Mundo</p>
+      <p>Bom dia</p>
+
+      <h2>Salários com aumentos</h2>
+      <ul>
+        {salComAumento.map((salario,i)=>(
+          <li key={i}>{salario}</li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
-
-export default App
